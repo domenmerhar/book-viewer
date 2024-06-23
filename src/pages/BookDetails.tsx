b22140c1e1df393d53df6185e1068d358578e936
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 14:42:56 +0200

    Update BookDetails.tsx

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,12 +1,16 @@
+import toast from "react-hot-toast";
 import { useBook } from "../hooks/useBook";
 import { SpinnerBig } from "../utils/SpinnerBig";
+import { HeadingGradient } from "../utils/HeadingGradient";
 
 export const BookDetails = () => {
   const { book, error, isLoading } = useBook();
 
   if (isLoading) return <SpinnerBig loading={isLoading} />;
 
+  if (error) return toast.error("An error occurred. Please try again later.");
+
   console.log(book);
 
-  return <div>BookDetails</div>;
+  return <HeadingGradient>{book.title}</HeadingGradient>;
 };
