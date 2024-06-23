9dbb73235d0ab3b34f13a1f1adaa7d621f08386d
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 12:18:17 +0200

    Add Navbar.tsx

---
+++ src/features/layout/Navbar.tsx
@@ -0,0 +1,50 @@
+import { Logo } from "../../utils/Logo";
+import { HiArchive } from "react-icons/hi";
+import { SearchBar } from "./SearchBar";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+const StyledNavbar = styled.nav`
+  background-color: var(--blue-3);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 70px;
+  padding: 0 20px;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+
+  & * {
+    transition: color 200ms ease-in;
+    transition: fill 200ms ease-in;
+  }
+
+  &.active * {
+    color: var(--gray-3) !important;
+    fill: var(--gray-3);
+  }
+
+  & :hover {
+    color: var(--gray-3);
+    fill: var(--gray-3);
+  }
+`;
+
+export const Navbar = () => {
+  return (
+    <StyledNavbar>
+      <StyledNavLink to="/books">
+        <Logo />
+      </StyledNavLink>
+
+      <SearchBar />
+
+      <StyledNavLink to="/your-books">
+        <HiArchive size={"48px"} fill="white" />
+      </StyledNavLink>
+    </StyledNavbar>
+  );
+};
