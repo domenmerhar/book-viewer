f67033b3c84a22db0b2a7977deb14a6672f5b1bc
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 16:30:10 +0200

    Update BookInfoCard.tsx

--- src/features/BookDetails/BookInfoCard.tsx
+++ src/features/BookDetails/BookInfoCard.tsx
@@ -1,64 +1,131 @@
+import styled from "styled-components";
 import { Book } from "../../Interface/Book";
 import { Column } from "../../utils/BookDetails";
 import { Heading } from "../../utils/Heading";
 import { Holder } from "../../utils/Holder";
 import { InfoParagraph } from "./InfoParagraph";
+import { HiDownload } from "react-icons/hi";
 
 interface BookInfoCardProps {
   book: Book | undefined;
 }
 
+const ButtonGradient = styled.button`
+  background: linear-gradient(55deg, var(--blue-5), var(--blue-8));
+  border: none;
+  border-radius: 1000px;
+  padding: 0.75em 1.25em;
+  font-size: 16px;
+  font-weight: 600;
+
+  transition: all 200ms;
+
+  & * {
+    color: var(--white);
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Select = styled.select`
+  background-color: var(--gray-1);
+  color: var(--black);
+  border: none;
+  padding: 4px 8px;
+  border-radius: 1000px;
+
+  margin-top: 2px;
+`;
+
+const Option = styled.option`
+  background-color: var(--gray-1);
+  color: var(--black);
+`;
+
 export const BookInfoCard: React.FC<BookInfoCardProps> = ({ book }) => {
   return (
-    <Holder width="370px" justifyContent="flex-start" alignItems="flex-start">
-      <Column gap="16px">
-        <span>
-          <InfoParagraph
-            title="Written By"
-            renderFn={() =>
-              book?.authors.map((author) => (
-                <Heading
-                  type="secondary"
-                  key={author.name}
-                  color="rgba(0, 0, 0, 0.75)"
-                >
-                  {author.name}
-                </Heading>
-              ))
-            }
-          />
-        </span>
-
-        <span>
-          <InfoParagraph
-            title="Themes"
-            renderFn={() =>
-              book?.subjects.map((subject) => {
-                const theme = subject.split("--").join(" ").split("(")[0];
-
-                return (
-                  <Heading type="secondary" key={subject + Date.now()}>
-                    {theme}
+    <Holder
+      width="370px"
+      justifyContent="space-between"
+      alignItems="flex-start"
+      isColumn={true}
+    >
+      <div>
+        <Column gap="16px">
+          <span>
+            <InfoParagraph
+              title="Written By"
+              renderFn={() =>
+                book?.authors.map((author) => (
+                  <Heading
+                    type="secondary"
+                    key={author.name}
+                    color="rgba(0, 0, 0, 0.75)"
+                  >
+                    {author.name}
                   </Heading>
-                );
-              })
-            }
-          />
-        </span>
+                ))
+              }
+            />
+          </span>
 
-        <span>
-          <InfoParagraph
-            title="Languages"
-            renderFn={() =>
-              book?.languages.map((language) => (
-                <Heading type="secondary" key={language}>
-                  {language}
-                </Heading>
-              ))
-            }
-          />
-        </span>
-      </Column>
+          <span>
+            <InfoParagraph
+              title="Themes"
+              renderFn={() =>
+                book?.subjects.map((subject) => {
+                  const theme = subject.split("--").join(" ").split("(")[0];
+
+                  return (
+                    <Heading type="secondary" key={subject + Date.now()}>
+                      {theme}
+                    </Heading>
+                  );
+                })
+              }
+            />
+          </span>
+
+          <span>
+            <InfoParagraph
+              title="Languages"
+              renderFn={() =>
+                book?.languages.map((language) => (
+                  <Heading type="secondary" key={language}>
+                    {language}
+                  </Heading>
+                ))
+              }
+            />
+          </span>
+        </Column>
+      </div>
+
+      <ButtonGradient>
+        <Flex>
+          <HiDownload size={24} />
+          Download
+        </Flex>
+      </ButtonGradient>
+
+      <ButtonGradient>
+        <Flex>
+          Add to
+          <Select>
+            <Option>Wishlist</Option>
+            <Option>Reading</Option>
+            <Option>Finished</Option>
+          </Select>
+        </Flex>
+      </ButtonGradient>
     </Holder>
   );
 };
