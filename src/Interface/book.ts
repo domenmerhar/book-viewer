1b01be3590b55b141b17d7af5b719957ea6ffa62
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 15:03:25 +0200

    Add book.ts

---
+++ src/Interface/book.ts
@@ -0,0 +1,26 @@
+export interface Person {
+  name: string;
+  birth_year: number | null;
+  death_year: number | null;
+}
+
+export interface Book {
+  id: number;
+  title: string;
+  authors: Person[];
+  translators: Person[];
+  subjects: string[];
+  bookshelves: string[];
+  languages: string[];
+  copyright: boolean;
+  media_type: string;
+  formats: { [key: string]: string };
+  download_count: number;
+}
+
+export interface BooksData {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Book[];
+}
