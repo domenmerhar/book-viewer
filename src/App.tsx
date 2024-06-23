8a457b2c12765b7574ad85aec076505c6ef4bc84
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 08:56:39 +0200

    Update App.tsx; Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/books" replace={true} />} />
+          <Route path="/books" element={<p>books</p>} />
+          <Route path="/books/:id" element={<p>/books/:id</p>} />
+          <Route path="/your-books" element={<p>/your-books</p>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
