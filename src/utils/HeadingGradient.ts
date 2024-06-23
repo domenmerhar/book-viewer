64193828012eaa24569de00c3acd12f81f6e0849
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 14:42:31 +0200

    Add Heading Gradient.ts

---
+++ src/utils/HeadingGradient.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeadingGradient = styled.h1`
+  background: linear-gradient(55deg, var(--blue-5), var(--blue-8));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+
+  font-weight: 800;
+  font-size: 40px;
+  letter-spacing: 2px;
+`;
