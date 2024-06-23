1c27f0eab614cdd7ce02257867260e1bf797419a
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 10:38:45 +0200

    Update App.tsx; Implement pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
 import { Layout } from "./features/layout/Layout";
 import { Toaster } from "react-hot-toast";
+import { Books } from "./pages/Books";
+import { BookDetails } from "./pages/BookDetails";
+import { YourBooks } from "./pages/YourBooks";
 
 function App() {
   return (
@@ -12,9 +15,9 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Navigate to="/books" replace={true} />} />
-            <Route path="/books" element={<p>books</p>} />
-            <Route path="/books/:id" element={<p>/books/:id</p>} />
-            <Route path="/your-books" element={<p>/your-books</p>} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/books/:id" element={<BookDetails />} />
+            <Route path="/your-books" element={<YourBooks />} />
           </Route>
 
           <Route path="*" element={<p>Invalid path</p>} />
