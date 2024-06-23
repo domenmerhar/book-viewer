f2d73843e85c2225cecfdc4e75ad8399cb2db10a
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 12:10:35 +0200

    Update Books.tsx

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,27 +1,9 @@
-import { useNavigate } from "react-router-dom";
-import { books } from "../data";
-import { Card } from "../features/Books/Card";
-import { List } from "../utils/List";
+import { BookList } from "../features/Books/BookList";
 
 export const Books = () => {
-  const navigate = useNavigate();
-
-  const render: () => React.ReactNode[] = () =>
-    books.results.map((book) => (
-      <li>
-        <Card
-          key={book.id}
-          title={book.title}
-          subTitle={book.authors[0].name}
-          imageSource={book.formats["image/jpeg"]}
-          onClick={() => navigate(`/books/${book.id}`)}
-        />
-      </li>
-    ));
-
   return (
     <div>
-      <List itemWidth={300} renderFn={render} />
+      <BookList />
     </div>
   );
 };
