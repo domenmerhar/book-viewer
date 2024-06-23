d6ccc8a1d82527dc9932ff3ae43e7f0d702a8188
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 09:25:36 +0200

    Update Layout.tsx; Styling

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,22 @@
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
+import { Logo } from "./utils/Logo";
 
 const StyledNavbar = styled.nav`
   background-color: var(--blue-3);
-
   display: flex;
+  align-items: center;
   justify-content: space-between;
+
+  height: 70px;
+  padding: 0 20px;
 `;
 
 export const Layout = () => {
   return (
     <>
       <StyledNavbar>
-        <p>BookViewer</p>
+        <Logo />
         <div>
           <input type="text" placeholder="Book title" />
           <input type="text" placeholder="Author" />
