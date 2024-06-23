5ba9d40f78be4464d4c9fc16b384988506429c3f
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 14:32:34 +0200

    Update BookDetails.tsx

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,3 +1,12 @@
+import { useBook } from "../hooks/useBook";
+import { SpinnerBig } from "../utils/SpinnerBig";
+
 export const BookDetails = () => {
+  const { book, error, isLoading } = useBook();
+
+  if (isLoading) return <SpinnerBig loading={isLoading} />;
+
+  console.log(book);
+
   return <div>BookDetails</div>;
 };
