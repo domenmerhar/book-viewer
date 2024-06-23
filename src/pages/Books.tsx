a6fec84f56c10739907a96954d42f917d4e4fa12
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 12:15:24 +0200

    Update Books.tsx

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,9 +1,17 @@
+import styled from "styled-components";
 import { BookList } from "../features/Books/BookList";
+
+const StyledResults = styled.div`
+  width: 100%;
+  height: 100%;
+
+  background-color: var(--gray-2);
+`;
 
 export const Books = () => {
   return (
-    <div>
+    <StyledResults>
       <BookList />
-    </div>
+    </StyledResults>
   );
 };
