009deb7dace83724096b920f9e07d524157056d6
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 12:20:41 +0200

    Update Layout.tsx

--- src/features/layout/Layout.tsx
+++ src/features/layout/Layout.tsx
@@ -2,11 +2,23 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import { Navbar } from "./Navbar";
 
+const StyledResults = styled.div`
+  min-width: 100vh;
+  min-height: 100vh;
+
+  max-width: 100%;
+  max-height: 100%;
+
+  background-color: var(--gray-2);
+`;
+
 export const Layout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <StyledResults>
+        <Outlet />
+      </StyledResults>
     </>
   );
 };
