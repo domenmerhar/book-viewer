c258fdd75e7df91fd4fe1a1814c29994b04bffa4
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 16:43:25 +0200

    Update Row.tsx

--- src/utils/Row.tsx
+++ src/utils/Row.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface RowProps {
   gap?: string;
+  justifyContent?: string;
 }
 
 export const Row = styled.div<RowProps>`
@@ -9,4 +10,6 @@ export const Row = styled.div<RowProps>`
   flex-direction: row;
 
   ${(props) => props.gap && `gap: ${props.gap};`}
+  ${(props) =>
+    props.justifyContent && `justify-content: ${props.justifyContent};`}
 `;
