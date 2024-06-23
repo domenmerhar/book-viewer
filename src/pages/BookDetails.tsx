610d5938bf5bea48b90ab0ee5db456c8600622e9
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 15:04:10 +0200

    Update BookDetails.tsx

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -2,15 +2,58 @@ import toast from "react-hot-toast";
 import { useBook } from "../hooks/useBook";
 import { SpinnerBig } from "../utils/SpinnerBig";
 import { HeadingGradient } from "../utils/HeadingGradient";
+import styled from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+interface HolderProps {
+  width?: string;
+  height?: string;
+}
+
+const Holder = styled.div<HolderProps>`
+  ${(props) => props.width && `width: ${props.width}`}
+  ${(props) => props.height && `width: ${props.height}`}
+
+
+  padding: 30px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+
+  margin-top: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--gray-1);
+`;
+
+const Image = styled.img`
+  width: 300px;
+`;
 
 export const BookDetails = () => {
   const { book, error, isLoading } = useBook();
 
-  if (isLoading) return <SpinnerBig loading={isLoading} />;
-
   if (error) return toast.error("An error occurred. Please try again later.");
+
+  if (isLoading) return <SpinnerBig loading={isLoading} />;
 
   console.log(book);
 
-  return <HeadingGradient>{book.title}</HeadingGradient>;
+  return (
+    <>
+      <Row>
+        <div>
+          <HeadingGradient>{book?.title}</HeadingGradient>
+          <Holder>
+            <Image src={book?.formats["image/jpeg"]} alt={book?.title} />
+          </Holder>
+        </div>
+      </Row>
+    </>
+  );
 };
