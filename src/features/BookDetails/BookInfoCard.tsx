c052ac087431323fbcd4fd1189d169cf624f2222
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 16:15:36 +0200

    Update BookInfoCard.tsx

--- src/features/BookDetails/BookInfoCard.tsx
+++ src/features/BookDetails/BookInfoCard.tsx
@@ -10,48 +10,54 @@ interface BookInfoCardProps {
 
 export const BookInfoCard: React.FC<BookInfoCardProps> = ({ book }) => {
   return (
-    <Holder width="370px">
-      <Column>
-        <InfoParagraph
-          title="Written By"
-          renderFn={() =>
-            book?.authors.map((author) => (
-              <Heading
-                type="secondary"
-                key={author.name}
-                color="rgba(0, 0, 0, 0.75)"
-              >
-                {author.name}
-              </Heading>
-            ))
-          }
-        />
-
-        <InfoParagraph
-          title="Themes"
-          renderFn={() =>
-            book?.subjects.map((subject) => {
-              const theme = subject.split("--").join(" ").split("(")[0];
-
-              return (
-                <Heading type="secondary" key={subject + Date.now()}>
-                  {theme}
+    <Holder width="370px" justifyContent="flex-start" alignItems="flex-start">
+      <Column gap="16px">
+        <span>
+          <InfoParagraph
+            title="Written By"
+            renderFn={() =>
+              book?.authors.map((author) => (
+                <Heading
+                  type="secondary"
+                  key={author.name}
+                  color="rgba(0, 0, 0, 0.75)"
+                >
+                  {author.name}
                 </Heading>
-              );
-            })
-          }
-        />
+              ))
+            }
+          />
+        </span>
 
-        <InfoParagraph
-          title="Languages"
-          renderFn={() =>
-            book?.languages.map((language) => (
-              <Heading type="secondary" key={language}>
-                {language}
-              </Heading>
-            ))
-          }
-        />
+        <span>
+          <InfoParagraph
+            title="Themes"
+            renderFn={() =>
+              book?.subjects.map((subject) => {
+                const theme = subject.split("--").join(" ").split("(")[0];
+
+                return (
+                  <Heading type="secondary" key={subject + Date.now()}>
+                    {theme}
+                  </Heading>
+                );
+              })
+            }
+          />
+        </span>
+
+        <span>
+          <InfoParagraph
+            title="Languages"
+            renderFn={() =>
+              book?.languages.map((language) => (
+                <Heading type="secondary" key={language}>
+                  {language}
+                </Heading>
+              ))
+            }
+          />
+        </span>
       </Column>
     </Holder>
   );
