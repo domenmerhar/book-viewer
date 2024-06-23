49912efd97f395bec075429adb54e7b75125a576
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 09:08:50 +0200

    Update main.tsx; Add styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import styled from "styled-components";
+
+const Body = styled.main`
+  max-width: 1160px;
+  margin: 0 auto;
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Body>
+      <App />
+    </Body>
   </React.StrictMode>
 );
