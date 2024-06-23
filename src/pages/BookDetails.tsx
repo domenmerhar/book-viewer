094082f0cdb65ff873f457c5ed34cb138b51df01
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 15:30:10 +0200

    Update BookDetails.tsx

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -3,32 +3,11 @@ import { useBook } from "../hooks/useBook";
 import { SpinnerBig } from "../utils/SpinnerBig";
 import { HeadingGradient } from "../utils/HeadingGradient";
 import styled from "styled-components";
+import { Holder } from "../utils/Holder";
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
-`;
-
-interface HolderProps {
-  width?: string;
-  height?: string;
-}
-
-const Holder = styled.div<HolderProps>`
-  ${(props) => props.width && `width: ${props.width}`}
-  ${(props) => props.height && `width: ${props.height}`}
-
-
-  padding: 30px;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-
-  margin-top: 30px;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--gray-1);
 `;
 
 const Image = styled.img`
@@ -46,13 +25,13 @@ export const BookDetails = () => {
 
   return (
     <>
+      <HeadingGradient>{book?.title}</HeadingGradient>
       <Row>
-        <div>
-          <HeadingGradient>{book?.title}</HeadingGradient>
-          <Holder>
-            <Image src={book?.formats["image/jpeg"]} alt={book?.title} />
-          </Holder>
-        </div>
+        <Holder>
+          <Image src={book?.formats["image/jpeg"]} alt={book?.title} />
+        </Holder>
+
+        <Holder width="370px">dummy</Holder>
       </Row>
     </>
   );
