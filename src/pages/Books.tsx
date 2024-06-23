ac15a41aa413127a62dc6751b7f05765f7cdfd61
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-23 11:55:40 +0200

    Update Books.tsx; Book rendering

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,3 +1,23 @@
+import { books } from "../data";
+import { Card } from "../features/Books/Card";
+import { List } from "../utils/List";
+
 export const Books = () => {
-  return <div>Books</div>;
+  const render: () => React.ReactNode[] = () =>
+    books.results.map((book) => (
+      <li>
+        <Card
+          key={book.id}
+          title={book.title}
+          subTitle={book.authors[0].name}
+          imageSource={book.formats["image/jpeg"]}
+        />
+      </li>
+    ));
+
+  return (
+    <div>
+      <List itemWidth={300} renderFn={render} />
+    </div>
+  );
 };
