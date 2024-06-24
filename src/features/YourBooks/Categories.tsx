f47fbaaa34bcfa6d1098ee21f8a8ff7d171626e7
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-24 19:33:46 +0200

    Update Categories.tsx

---
+++ src/features/YourBooks/Categories.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Row } from "../../utils/Row";
+import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: var(--gray-3);
+  border: none;
+  border-radius: 10px;
+  text-transform: uppercase;
+  color: var(--black);
+  font-size: 14px;
+  padding: 10px 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: background-color 200ms;
+
+  &:disabled {
+    background-color: var(--blue-5);
+    color: var(--white);
+
+    &:hover {
+      cursor: not-allowed;
+    }
+  }
+`;
+
+interface CategoryProps {
+  categories: string[];
+}
+
+export const Categories: React.FC<CategoryProps> = ({ categories }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleClick = (categoryName: string) => {
+    setSearchParams({ category: categoryName });
+  };
+
+  const currentCategory = searchParams.get("category") || categories[0];
+
+  return (
+    <Row>
+      {categories.map((categoryItem) => {
+        console.log(currentCategory);
+        console.log(categoryItem);
+        console.log(currentCategory === categoryItem);
+
+        return (
+          <Button
+            key={categoryItem}
+            onClick={() => handleClick(categoryItem)}
+            disabled={currentCategory === categoryItem}
+          >
+            {categoryItem[0].toUpperCase() + categoryItem.slice(1)}
+          </Button>
+        );
+      })}
+    </Row>
+  );
+};
