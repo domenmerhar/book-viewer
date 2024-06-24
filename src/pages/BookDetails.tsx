af6ca67fec797ebb72e99965e78cd65069c63745
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-24 16:23:42 +0200

    Update BookDetailst.tsx

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -7,6 +7,29 @@ import { Holder } from "../utils/Holder";
 import { BookInfoCard } from "../features/BookDetails/BookInfoCard";
 import { Row } from "../utils/Row";
 
+interface StatusTagProps {
+  type: "wishlist" | "reading" | "finished";
+}
+
+const StatusTag = styled.span<StatusTagProps>`
+  ${(props) => props.type === "wishlist" && "background: var(--blue-5);"}
+  ${(props) => props.type === "reading" && "background: var(--blue-5);"}
+  ${(props) => props.type === "finished" && "background: var(--blue-5);"}
+
+  color: var(--white);
+  font-size: 12px;
+  letter-spacing: 3px;
+  padding: 8px 8px;
+  border-radius: 1000px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  text-transform: uppercase;
+  font-weight: 800;
+`;
+
 const Image = styled.img`
   width: 300px;
 `;
@@ -20,7 +43,12 @@ export const BookDetails = () => {
 
   return (
     <>
-      <HeadingGradient>{book?.title}</HeadingGradient>
+      <Row>
+        <HeadingGradient>{book?.title}</HeadingGradient>
+        <StatusTag type="wishlist">Wishlist</StatusTag>
+        <StatusTag type="reading">Reading</StatusTag>
+        <StatusTag type="finished">Finished</StatusTag>
+      </Row>
       <Row gap="64px" justifyContent="space-around">
         <Holder>
           <Image src={book?.formats["image/jpeg"]} alt={book?.title} />
