ca3f22758eb96f01a870675dba8b097c659904f7
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-24 19:09:38 +0200

    Update YourBooks.tsx

--- src/pages/YourBooks.tsx
+++ src/pages/YourBooks.tsx
@@ -3,9 +3,12 @@ import { LocalBook } from "../hooks/useLocalStorageState";
 import { useSavedBooks } from "../hooks/useSavedBooks";
 import { List } from "../utils/List";
 import { SpinnerBig } from "../utils/SpinnerBig";
+import { SelectSearch } from "../utils/SelectSearch";
+import { useSearchParams } from "react-router-dom";
 
 export const YourBooks = () => {
   const { data: savedBooks, error, isLoading } = useSavedBooks("wishlist");
+  const [searchParams, setSearchParams] = useSearchParams();
 
   if (error) {
     toast.error("An error occurred, please try again later.");
@@ -13,17 +16,38 @@ export const YourBooks = () => {
   }
   if (isLoading) return <SpinnerBig loading={true} />;
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSearchParams({ sort: e.target.value });
+  };
+
+  const options = [
+    "added (oldest first)",
+    "added (newest first)",
+    "youngest",
+    "oldest",
+    "title",
+    "author",
+  ];
+
   return (
-    <List
-      itemWidth={300}
-      renderFn={() =>
-        savedBooks.map((book: LocalBook) => (
-          <div>
-            <p>{book.id}</p>
-            <p>{book.collection}</p>
-          </div>
-        ))
-      }
-    />
+    <>
+      <SelectSearch
+        defaultValue="added (oldest first)"
+        values={options}
+        onChange={handleChange}
+      />
+
+      <List
+        itemWidth={300}
+        renderFn={() =>
+          savedBooks.map((book: LocalBook) => (
+            <div>
+              <p>{book.id}</p>
+              <p>{book.collection}</p>
+            </div>
+          ))
+        }
+      />
+    </>
   );
 };
