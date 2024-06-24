f46f3fac48ca7727cd10d731df478c5b4b6bd7e0
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-24 18:49:00 +0200

    Update YourBook.tsx

--- src/pages/YourBooks.tsx
+++ src/pages/YourBooks.tsx
@@ -1,3 +1,29 @@
+import toast from "react-hot-toast";
+import { LocalBook } from "../hooks/useLocalStorageState";
+import { useSavedBooks } from "../hooks/useSavedBooks";
+import { List } from "../utils/List";
+import { SpinnerBig } from "../utils/SpinnerBig";
+
 export const YourBooks = () => {
-  return <div>YourBooks</div>;
+  const { data: savedBooks, error, isLoading } = useSavedBooks("wishlist");
+
+  if (error) {
+    toast.error("An error occurred, please try again later.");
+    return null;
+  }
+  if (isLoading) return <SpinnerBig loading={true} />;
+
+  return (
+    <List
+      itemWidth={300}
+      renderFn={() =>
+        savedBooks.map((book: LocalBook) => (
+          <div>
+            <p>{book.id}</p>
+            <p>{book.collection}</p>
+          </div>
+        ))
+      }
+    />
+  );
 };
