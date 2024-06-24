22245e7c0e400393026375dab12bf847691bc4a5
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-24 21:12:06 +0200

    Update CardSmall.tsx; styling

--- src/features/YourBooks/CardSmall.tsx
+++ src/features/YourBooks/CardSmall.tsx
@@ -3,6 +3,7 @@ import { Holder } from "../../utils/Holder";
 import { Row } from "../../utils/Row";
 import styled from "styled-components";
 import { Heading } from "../../utils/Heading";
+import { Column } from "../../utils/BookDetails";
 
 interface CardSmallProps {
   image: string;
@@ -14,6 +15,10 @@ const Image = styled.img`
   width: 150px;
 `;
 
+const InfoWrapper = styled.div`
+  margin-top: 4px;
+`;
+
 export const CardSmall: React.FC<CardSmallProps> = ({
   image,
   title,
@@ -21,10 +26,14 @@ export const CardSmall: React.FC<CardSmallProps> = ({
 }) => {
   return (
     <Holder width="415px">
-      <Row>
+      <Row gap="16px">
         <Image src={image} alt={title} />
-        <Heading type="primary">{title}</Heading>
-        <Heading type="secondary">{author}</Heading>
+        <InfoWrapper>
+          <Column gap="8px">
+            <Heading type="primary">{title}</Heading>
+            <Heading type="secondary">{author}</Heading>
+          </Column>
+        </InfoWrapper>
       </Row>
     </Holder>
   );
