4f0fd69539912d557da17925367d4c3a3a214554
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 09:32:47 +0200

    Add ButtonSquare.tsx

---
+++ src/utils/ButtonSquare.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonSquare = styled.button`
+  background-color: var(--gray-5);
+
+  min-width: 48px;
+  max-width: 48px;
+
+  min-height: 48px;
+  max-height: 48px;
+  border-radius: 10px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+`;
