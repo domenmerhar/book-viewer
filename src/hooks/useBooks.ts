e115c2c0b6184b59665e5af5c309186c4e27d43d
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 16:36:36 +0200

    Update useBook.ts

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,8 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBooks } from "../api/getBooks";
 import { BooksData } from "../Interface/Book";
+import { useSearchParams } from "react-router-dom";
 
 export const useBooks = () => {
+  const [searchParams] = useSearchParams();
+
+  const title = searchParams.get("title") || "";
+  const author = searchParams.get("author") || "";
+
   const {
     data: books,
     error,
@@ -12,8 +18,8 @@ export const useBooks = () => {
     error: Error | null;
     isLoading: boolean;
   } = useQuery({
-    queryKey: ["books"],
-    queryFn: getBooks,
+    queryKey: ["books", title, author],
+    queryFn: () => getBooks({ title, author }),
     staleTime: Infinity,
   });
 
