65a910fb6897457469755ac480d0e027a89cc624
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 20:04:20 +0200

    Update getBooks.ts

--- src/api/getBooks.ts
+++ src/api/getBooks.ts
@@ -6,13 +6,15 @@ interface TitleAuthor {
   page: number;
 }
 
-export const getBooks = async ({ title, author }: TitleAuthor) => {
+export const getBooks = async ({ title, author, page }: TitleAuthor) => {
+  console.log(page);
   let url = `${GUTENDEX_URL}books/`;
 
   if (title || author)
     url = `${url}?search=${title.toLowerCase()}${
-      author && `%20${author.toLowerCase()}`
+      author && `%20${author.toLowerCase()}&page=${page}`
     }`;
+  else url = `${url}?page=${page}`;
 
   const response = await fetch(url);
   const data = await response.json();
