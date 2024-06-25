4bd689bf7cf86f377346e42e27f6585067220c8f
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 11:26:54 +0200

    Update CardSmall.tsx

--- src/features/YourBooks/CardSmall.tsx
+++ src/features/YourBooks/CardSmall.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Holder } from "../../utils/Holder";
 import { Row } from "../../utils/Row";
 import styled from "styled-components";
@@ -7,8 +7,10 @@ import { Column } from "../../utils/BookDetails";
 import ExpandingList from "../../utils/ExpandingList";
 import { ButtonSquare } from "../../utils/ButtonSquare";
 import { HiOutlineDotsVertical } from "react-icons/hi";
+import { DropDownList } from "./DropDownList";
 
 interface CardSmallProps {
+  id: number;
   image: string;
   title: string;
   author: string;
@@ -23,33 +25,32 @@ const InfoWrapper = styled.div`
 `;
 
 export const CardSmall: React.FC<CardSmallProps> = ({
+  id,
   image,
   title,
   author,
 }) => {
   return (
-    <ExpandingList>
-      <Holder width="415px">
-        <Row gap="16px">
-          <Image src={image} alt={title} />
-          <InfoWrapper>
-            <Column gap="8px">
-              <Heading type="primary">{title}</Heading>
-              <Heading type="secondary">{author}</Heading>
-            </Column>
-          </InfoWrapper>
+    <Holder width="415px">
+      <Row gap="16px">
+        <Image src={image} alt={title} />
+        <InfoWrapper>
+          <Column gap="8px">
+            <Heading type="primary">{title}</Heading>
+            <Heading type="secondary">{author}</Heading>
+          </Column>
+        </InfoWrapper>
 
-          <ExpandingList.Button>
-            <ButtonSquare>
-              <HiOutlineDotsVertical size={24} />
-            </ButtonSquare>
-          </ExpandingList.Button>
+        <ExpandingList.Button>
+          <ButtonSquare>
+            <HiOutlineDotsVertical size={24} />
+          </ButtonSquare>
+        </ExpandingList.Button>
 
-          <ExpandingList.List>
-            <p>list</p>
-          </ExpandingList.List>
-        </Row>
-      </Holder>
-    </ExpandingList>
+        <ExpandingList.List>
+          <DropDownList id={id} />
+        </ExpandingList.List>
+      </Row>
+    </Holder>
   );
 };
