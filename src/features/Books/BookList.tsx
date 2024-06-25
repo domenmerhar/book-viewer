e999ae33cfa9174bc4e90d30c35daff272815bf4
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 19:40:15 +0200

    Update BookList.tsx

--- src/features/Books/BookList.tsx
+++ src/features/Books/BookList.tsx
@@ -4,26 +4,30 @@ import { List } from "../../utils/List";
 import { useBooks } from "../../hooks/useBooks";
 import toast from "react-hot-toast";
 import { SpinnerBig } from "../../utils/SpinnerBig";
+import { BooksData } from "../../Interface/Book";
 
 export const BookList = () => {
-  const { books, error, isLoading } = useBooks();
+  const { data, error, isLoading } = useBooks();
 
   if (error) return toast.error("An error occurred");
 
   if (isLoading) return <SpinnerBig loading={isLoading} />;
 
+  const pages = data?.pages;
+
   const render: () => React.ReactNode[] = () =>
-    books!.results.map((book) => (
-      <li>
-        <Card
-          key={book.id}
-          title={book.title}
-          subTitle={book.authors[0].name}
-          imageSource={book.formats["image/jpeg"]}
-          to={`/books/${book.id}`}
-        />
-      </li>
-    ));
+    pages!.map((books: BooksData) =>
+      books!.results.map((book) => (
+        <li key={book.id}>
+          <Card
+            title={book.title}
+            subTitle={book.authors[0].name}
+            imageSource={book.formats["image/jpeg"]}
+            to={`/books/${book.id}`}
+          />
+        </li>
+      ))
+    );
 
   return <List itemWidth={300} renderFn={render} />;
 };
