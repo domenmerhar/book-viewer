be86dc5bcff487d6786193ff17ba0ba18981d8b4
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 09:56:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import styled from "styled-components";
+import { YourBooksProvider } from "./hooks/useYourBooks.tsx";
 
 const Body = styled.main`
   max-width: 1160px;
@@ -10,8 +11,10 @@ const Body = styled.main`
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Body>
-      <App />
-    </Body>
+    <YourBooksProvider>
+      <Body>
+        <App />
+      </Body>
+    </YourBooksProvider>
   </React.StrictMode>
 );
