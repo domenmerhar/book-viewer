474254f67e0becd25ed05d38ada972ddf0804fd4
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-25 09:33:00 +0200

    Update CardSmall.tsx

--- src/features/YourBooks/CardSmall.tsx
+++ src/features/YourBooks/CardSmall.tsx
@@ -4,6 +4,9 @@ import { Row } from "../../utils/Row";
 import styled from "styled-components";
 import { Heading } from "../../utils/Heading";
 import { Column } from "../../utils/BookDetails";
+import ExpandingList from "../../utils/ExpandingList";
+import { ButtonSquare } from "../../utils/ButtonSquare";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
 interface CardSmallProps {
   image: string;
@@ -25,16 +28,28 @@ export const CardSmall: React.FC<CardSmallProps> = ({
   author,
 }) => {
   return (
-    <Holder width="415px">
-      <Row gap="16px">
-        <Image src={image} alt={title} />
-        <InfoWrapper>
-          <Column gap="8px">
-            <Heading type="primary">{title}</Heading>
-            <Heading type="secondary">{author}</Heading>
-          </Column>
-        </InfoWrapper>
-      </Row>
-    </Holder>
+    <ExpandingList>
+      <Holder width="415px">
+        <Row gap="16px">
+          <Image src={image} alt={title} />
+          <InfoWrapper>
+            <Column gap="8px">
+              <Heading type="primary">{title}</Heading>
+              <Heading type="secondary">{author}</Heading>
+            </Column>
+          </InfoWrapper>
+
+          <ExpandingList.Button>
+            <ButtonSquare>
+              <HiOutlineDotsVertical size={24} />
+            </ButtonSquare>
+          </ExpandingList.Button>
+
+          <ExpandingList.List>
+            <p>list</p>
+          </ExpandingList.List>
+        </Row>
+      </Holder>
+    </ExpandingList>
   );
 };
