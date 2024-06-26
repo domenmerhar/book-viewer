a3b91df38ee161fa051a3297d61b082d8474881f
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-26 10:06:54 +0200

    Updat BookList.tsx

--- src/features/Books/BookList.tsx
+++ src/features/Books/BookList.tsx
@@ -1,38 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Card } from "./Card";
 import { List } from "../../utils/List";
-import { useBooks } from "../../hooks/useBooks";
 import toast from "react-hot-toast";
 import { SpinnerBig } from "../../utils/SpinnerBig";
-import { BooksData } from "../../Interface/Book";
-import { useInView } from "react-intersection-observer";
+import { BooksData } from "../../Interface/book";
 import { Heading } from "../../utils/Heading";
-
-interface InvalidPage {
-  detail: "Invalid page.";
-}
+import { useBookList } from "./BookList.hooks";
 
 export const BookList = () => {
-  const { data, error, isLoading, fetchNextPage } = useBooks();
-  const { ref, inView } = useInView();
-
-  const invalidPage =
-    data?.pages?.[0]?.count === undefined ||
-    data?.pages?.[0]?.count === 0 ||
-    (data?.pages[data?.pages.length - 1] as unknown as InvalidPage)?.detail ===
-      "Invalid page.";
-
-  useEffect(() => {
-    if (inView && !invalidPage) {
-      fetchNextPage();
-    }
-  }, [inView, fetchNextPage, invalidPage]);
+  const { data, pages, error, isLoading, invalidPage, ref, inView } =
+    useBookList();
 
   if (error) return toast.error("An error occurred");
-
   if (isLoading) return <SpinnerBig loading={isLoading} />;
-
-  const pages = data?.pages;
 
   if (invalidPage && data?.pages.length === 1) return <h1>No results found</h1>;
 
