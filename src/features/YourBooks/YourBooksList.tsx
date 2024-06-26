1023e1de270c4aafbe0d755cc9c16245ba66e833
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-26 10:13:09 +0200

    Update YourBooksList.tsx

--- src/features/YourBooks/YourBooksList.tsx
+++ src/features/YourBooks/YourBooksList.tsx
@@ -1,46 +1,14 @@
 import { List } from "../../utils/List";
 import { CardSmall } from "./CardSmall";
-import { Book } from "../../Interface/Book";
-import { useSavedBooks } from "../../hooks/useSavedBooks";
-import toast from "react-hot-toast";
-import { sortBooks, sortType } from "../../utils/sortBooks";
+import { Book } from "../../Interface/book";
 import { SpinnerBig } from "../../utils/SpinnerBig";
-import { useSearchParams } from "react-router-dom";
-import { useYourBooks } from "../../hooks/useYourBooks";
-
-const options = [
-  "added (oldest first)",
-  "added (newest first)",
-  "oldest",
-  "youngest",
-  "title",
-  "author",
-];
-
-type category = "wishlist" | "reading" | "finished";
+import { useYourBooksList } from "./YourBooksList.hooks";
 
 export const YourBooksList = () => {
-  const [searchParams] = useSearchParams();
-  const { savedBooks: localBooks } = useYourBooks();
+  const { isLoading, sortedBooks } = useYourBooksList();
 
-  const category = (searchParams.get("category") as category) || "wishlist";
-  const currentCategory: category = category;
-
-  const {
-    data: savedBooks,
-    error,
-    isLoading,
-  } = useSavedBooks(currentCategory, localBooks);
-
-  const sort: sortType = (searchParams.get("sort") as sortType) || options[0];
-
-  if (error) {
-    toast.error("An error occurred, please try again later.");
-    return null;
-  }
   if (isLoading) return <SpinnerBig loading={true} />;
 
-  const sortedBooks = sortBooks(savedBooks, sort);
   return (
     <List
       itemWidth={415}
