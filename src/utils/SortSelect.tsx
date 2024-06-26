3c0e028353eb5802d0a4dc79556be4c12903bb8f
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-26 10:51:56 +0200

    Add SortSelect.tsx

---
+++ src/utils/SortSelect.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Row } from "./Row";
+import { Heading } from "./Heading";
+import { SelectSearch } from "./SelectSearch";
+
+interface Options {
+  value: string;
+  text?: string;
+}
+
+interface SortSelectParams {
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: Options[];
+  defaultValue: string;
+}
+
+export const SortSelect: React.FC<SortSelectParams> = ({
+  onChange,
+  options,
+  defaultValue,
+}) => {
+  return (
+    <Row gap="16px" alignItems="center">
+      <Heading type="secondary">Sort by: </Heading>
+      <SelectSearch
+        defaultValue={defaultValue}
+        values={options}
+        onChange={onChange}
+      />
+    </Row>
+  );
+};
