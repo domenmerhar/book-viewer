fa8f5eb622b07cbc78c6b36691fb0f4862609e9d
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-26 10:58:04 +0200

    Update Books.tsx; Add filter

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,5 +1,45 @@
+import { useSearchParams } from "react-router-dom";
+import { SortSelect } from "../utils/SortSelect";
 import { BookList } from "../features/Books/BookList";
+import { Row } from "../utils/Row";
+import styled from "styled-components";
+
+interface Option {
+  value: string;
+  text?: string;
+}
+
+const options: Option[] = [
+  { value: "popular", text: "Most popular" },
+  { value: "ascending", text: "ID (ascending)" },
+  { value: "descending", text: "ID (descending)" },
+];
+
+const SelectHolder = styled.div`
+  margin-bottom: 24px;
+`;
 
 export const Books = () => {
-  return <BookList />;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const defaultValue = searchParams.get("sort") || "Most popular";
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const params = Object.fromEntries(searchParams.entries());
+    setSearchParams({ ...params, sort: e.target.value });
+  };
+
+  return (
+    <>
+      <SelectHolder>
+        <Row justifyContent="flex-end">
+          <SortSelect
+            defaultValue={defaultValue}
+            onChange={handleChange}
+            options={options}
+          />
+        </Row>
+      </SelectHolder>
+      <BookList />
+    </>
+  );
 };
