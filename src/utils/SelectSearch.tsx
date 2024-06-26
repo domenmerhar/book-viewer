2f0852c4ee69eaaebb3f3c03157a098a3005b9d2
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-26 10:51:35 +0200

    Update SelectSearch.tsx

--- src/utils/SelectSearch.tsx
+++ src/utils/SelectSearch.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { AddOption } from "../features/BookDetails/AddOption";
 import styled from "styled-components";
 
+interface Options {
+  value: string;
+  text?: string;
+}
+
 interface SelectSearchParams {
   onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  values: string[];
+  values: Options[];
   defaultValue: string;
 }
 
@@ -25,8 +30,8 @@ export const SelectSearch: React.FC<SelectSearchParams> = ({
 }) => {
   return (
     <Select onChange={onChange} defaultValue={defaultValue}>
-      {values.map((value) => (
-        <AddOption key={value} collection={value} />
+      {values.map(({ value, text }) => (
+        <AddOption key={value} value={value} text={text} />
       ))}
     </Select>
   );
