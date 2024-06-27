77afe064b61674851d96cdff4341e93925229acb
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-27 10:36:39 +0200

    Update useBooks.ts

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -1,13 +1,17 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getBooks } from "../api/getBooks";
 import { BooksData } from "../Interface/book";
-import { useSearchParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 export const useBooks = () => {
   const [searchParams] = useSearchParams();
+  const { id } = useParams<{ id: string }>();
 
   const title = searchParams.get("title") || "";
-  const author = searchParams.get("author") || "";
+  const author =
+    searchParams.get("author") ||
+    id?.split("=")[1].split(" ").join("+").replace(",", "") ||
+    "";
   const sort =
     (searchParams.get("sort") as "popular" | "ascending" | "descending") ||
     "popular";
@@ -27,7 +31,9 @@ export const useBooks = () => {
     queryFn: ({ pageParam }: { pageParam: number }) =>
       getBooks({ title, author, sort, page: pageParam }),
     initialPageParam: 1,
-    getNextPageParam: (lastPage) => lastPage.next,
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || "2";
+    },
   });
 
   return { data, error, isLoading, fetchNextPage };
