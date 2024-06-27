76523973c88378d7bf795b6ad3b1cbd719765b72
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-27 12:27:36 +0200

    Update YourBooks.tsx; Add error boundary

--- src/pages/YourBooks.tsx
+++ src/pages/YourBooks.tsx
@@ -1,11 +1,15 @@
+import { ErrorBoundary } from "react-error-boundary";
 import { SortFilter } from "../features/YourBooks/SortFilter";
 import { YourBooksList } from "../features/YourBooks/YourBooksList";
+import { ErrorFallback } from "../utils/ErrorFallback";
 
 export const YourBooks = () => {
   return (
     <>
-      <SortFilter />
-      <YourBooksList />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <SortFilter />
+        <YourBooksList />
+      </ErrorBoundary>
     </>
   );
 };
