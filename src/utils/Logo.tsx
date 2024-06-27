43abbefcb69a8d084c9ef0e296b8d36d0f08746c
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-27 11:48:24 +0200

    Update Logo.tsx; Rename prop

--- src/utils/Logo.tsx
+++ src/utils/Logo.tsx
@@ -4,13 +4,13 @@ import styled from "styled-components";
 interface LogoProps {
   fontSize?: string;
   paddingx?: string;
-  paddingY?: string;
+  paddingy?: string;
 }
 
 const StyledLogo = styled.p<LogoProps>`
   font-size: ${(props) => props.fontSize};
 
-  padding: ${(props) => `${props.paddingY} ${props.paddingx}`};
+  padding: ${(props) => `${props.paddingy} ${props.paddingx}`};
 
   font-family: "Pirata one", sans-serif;
   color: white;
@@ -19,10 +19,10 @@ const StyledLogo = styled.p<LogoProps>`
 export const Logo: React.FC<LogoProps> = ({
   fontSize = "32px",
   paddingx = "0px",
-  paddingY = "0px",
+  paddingy = "0px",
 }) => {
   return (
-    <StyledLogo fontSize={fontSize} paddingx={paddingx} paddingY={paddingY}>
+    <StyledLogo fontSize={fontSize} paddingx={paddingx} paddingy={paddingy}>
       BookViewer
     </StyledLogo>
   );
