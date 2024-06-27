3e3560069e9815561eb214a5d982d54bc0551267
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-27 12:26:38 +0200

    Update main.tsx; Add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import styled from "styled-components";
 import { YourBooksProvider } from "./hooks/useYourBooks.tsx";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./utils/ErrorFallback.tsx";
 
 const Body = styled.main`
   max-width: 1160px;
@@ -11,10 +13,12 @@ const Body = styled.main`
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <YourBooksProvider>
-      <Body>
-        <App />
-      </Body>
-    </YourBooksProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <YourBooksProvider>
+        <Body>
+          <App />
+        </Body>
+      </YourBooksProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
