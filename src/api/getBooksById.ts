ced7bf74ca83f5557432e8ff113ad3a35a504401
Domen Merhar <domen.merhar@students.vegova.si>
2024-06-27 11:15:48 +0200

    Update getBookById.ts; Update Types

--- src/api/getBooksById.ts
+++ src/api/getBooksById.ts
@@ -1,6 +1,6 @@
 import { GUTENDEX_URL } from "./config";
 
-export const getBooksById = async (...ids: string[]) => {
+export const getBooksById = async (ids: string[]) => {
   const response = await fetch(`${GUTENDEX_URL}/books/?ids=${ids.join(",")}`);
   const data = await response.json();
   return data;
